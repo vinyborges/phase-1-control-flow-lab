c6c72716009f2a242f98f21b5c817acd1eecbc4a
vinyborges <viny.bdj@gmail.com>
2023-01-07 03:00:43 -0800

    commit tree 8ae54dc436cc1e3619041eb6ee244485a48c1c7d

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(someNumber){
+  let result;
+  if (someNumber<=400){
+  result = 'This one is on me!';
+  }
+    else if (someNumber >400 && someNumber<2000){
+      result = 'That will be twenty bucks.';
+  }
+    else if (someNumber >2000 && someNumber <2500){
+      result = 'I will gladly take your thirty bucks.';
+}
+    else if (someNumber >2500){
+      result = 'No can do.';
+    }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let msg;
+  if (city != "NYC"){
+    msg = "No go.";
+  } else if (city = 'NYC'){
+    msg = 'Ok, sounds good.'
+  }
+
+  return msg;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result;
+  switch (tip){
+  case 'not as generous': 
+  result = 'Thank you.';
+  break;
+  case 'generous': 
+  result ='Thank you so much.'; 
+  break;
+  default: 
+  result = 'Bye.';
+
+  }
+
+  return result;
 }
